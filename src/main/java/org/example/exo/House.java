d8f982bf5b71261bcf782f014b7e2f50ce9cf406
Anthoine <59anth59@gmail.com>
2025-05-20 14:56:44 +0200

    d pattern

--- src/main/java/org/example/exo/House.java
+++ src/main/java/org/example/exo/House.java
@@ -9,42 +9,44 @@ public class House {
     private String roofType;
     private String color;
 
-    public House(Builder builder){
+    public House(Builder builder) {
         stage = builder.stage;
         pool = builder.pool;
         roofType = builder.roofType;
         color = builder.color;
-    }
 
-    public static class Builder {
-        private int stage;
-        private boolean pool;
-        private String roofType;
-        private String color;
-    }
 
-    public Builder (int stage) {
-        this.stage = stage;
-        return this;
-    }
+        public static class Builder {
+            private int stage;
+            private boolean pool;
+            private String roofType;
+            private String color;
+        }
 
-    public Builder(boolean pool){
-        this.pool = pool;
-        return this;
-    }
+    public Builder( int stage){
+            this.stage = stage;
+            return this;
+        }
 
-    public Builder(String roofType){
-        this.roofType=roofType;
-        return this;
-    }
+    public Builder( boolean pool){
+            this.pool = pool;
+            return this;
+        }
 
-    public Builder(String color){
-        this.color=color;
-        return this;
-    }
+    public Builder(String roofType) {
+            this.roofType = roofType;
+            return this;
+        }
 
-    public House build(){
-        return new House(this);
+    public Builder(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public House build () {
+            return new House(this);
+        }
+
     }
 
     @Override
